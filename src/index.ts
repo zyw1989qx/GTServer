d84d1f3a77cf765cc247af61aba901f5ffbfed53
zyw <zyw.1989.qx@gmail.com>
2018-08-16 23:21:29 -0400

    First time commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "reflect-metadata";
+import { importSchema } from "graphql-import";
+import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
+import * as path from 'path';
+const typeDefs = importSchema(path.join(__dirname,"./schema.graphql"));
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
---
+++ src/resolvers.ts
@@ -0,0 +1,12 @@
+import { ResolverMap } from "./types/graphql-utils";
+
+export const resolvers: ResolverMap = {
+  Query: {
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
+  },
+  Mutation:{
+    register:(_,{email,password}:GQL.IRegisterOnMutationArguments)=>{
+      return email+password;
+    }
+  }
+};
